3d71b914d17a4902d432dd62d7aac3ccc6c80ce8
fredlun <fred.lunden@authority.se>
2022-12-02 11:18:31 +0100

    init commit

---
+++ d_01/Program.fs
@@ -0,0 +1,37 @@
+﻿open System.IO
+open System
+
+let split (separators: string) (x: string) = x.Split(separators)
+
+let parse (input: string[]) =
+    input
+    |> String.concat "|"
+    |> split "||"
+    |> Seq.map (split "|")
+    |> Seq.map (fun x -> x |> Seq.map int)
+
+let q1 (input: string[]) =
+    let elfs = input |> parse
+    let cals = elfs |> Seq.map Seq.sum
+    let maxCals = cals |> Seq.max
+
+    maxCals
+
+let q2 (input: string[]) =
+    let elfs = input |> parse
+    let cals = elfs |> Seq.map Seq.sum
+    let maxCals =
+        cals
+        |> Seq.sortDescending
+        |> Seq.take 3
+        |> Seq.sum
+
+    maxCals
+
+let input = File.ReadAllLines("input_01.txt")
+
+let result1 = input |> q1
+Console.WriteLine(result1)
+
+let result2 = input |> q2
+Console.WriteLine(result2)
