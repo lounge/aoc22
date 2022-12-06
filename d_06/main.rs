4c9b3a56a30f1831034c4862bc9a7d56ffde0384
lounge <fredlunden@gmail.com>
2022-12-06 18:42:28 +0100

    cleanup

--- d_06/main.rs
+++ d_06/main.rs
@@ -2,23 +2,20 @@ use std::fs;
 use itertools::Itertools;
 
 fn main() {
-    const START_COUNT: usize = 4;
-    const MSG_COUNT: usize = 14;
+    fn get_pos(msg_count: usize) -> usize  {
+        let mut size = 0;
+        let contents = fs::read_to_string("input_01.txt").expect("File should be read");
 
-    let contents = fs::read_to_string("input_01.txt").expect("File should be read");
+        for (i, _) in contents.chars().enumerate() {
+            let chunk = &contents.get(i..i+msg_count).expect("Chunk should not be Empty");
+            if chunk.chars().unique().count() == msg_count {
+                size = &i + msg_count;
+                break;
+            }                 
+        }
+        size
+    }
 
-    for (i, _) in contents.chars().enumerate() {
-        let chunk = &contents.get(i..i+START_COUNT).expect("Chunk should not be Empty");
-        if chunk.chars().unique().count() == START_COUNT {
-            println!("q1: {}", &i + START_COUNT);
-            break;
-        }                 
-    }
-    for (i, _) in contents.chars().enumerate() {
-        let chunk = &contents.get(i..i+MSG_COUNT).expect("Chunk should not be Empty");
-        if chunk.chars().unique().count() == MSG_COUNT {
-            println!("q2: {}", &i + MSG_COUNT);
-            break;
-        }                 
-    }
+    println!("q1: {}", get_pos(4));
+    println!("q2: {}", get_pos(14));
 }
